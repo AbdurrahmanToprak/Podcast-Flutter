531740e3baa706a693bad6bfed25e610e39e4285
Abdurrahman TOPRAK <toprakelz23@gmail.com>
2024-11-13 22:28:16 +0300

    podcast detail page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,64 +15,151 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  final PageController _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 20, 10, 64),
-      body: Stack(
-        children: [
-          Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30.0),
-                    topRight: Radius.circular(30.0),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              height: 250,
+              width: double.infinity,
+              child: PageView(
+                controller: _pageController,
+                children: [
+                  buildCard(
+                    imagePath: 'assets/images/podcast.png',
+                    description: 'Podkes - Eğlenceli sohbetler burada!',
                   ),
-                  child: Image.asset(
-                    'assets/images/podcast.png',
-                    width: 300,
-                    height: 150,
-                    fit: BoxFit.cover,
+                  buildCard(
+                    imagePath: 'assets/images/podcast1.jpg',
+                    description: 'Podkes - En güncel haberler sizlerle.',
                   ),
-                ),
-                SizedBox(height: 10),
-                Text(
-                  "Podkes",
-                  style: TextStyle(color: Colors.white, fontSize: 24),
-                ),
-              ],
+                  buildCard(
+                    imagePath: 'assets/images/podcast2.jpg',
+                    description: 'Podkes - Günlük motivasyon konuşmaları.',
+                  ),
+                ],
+              ),
             ),
-          ),
-          Align(
-            alignment: Alignment(0, 0.9),
-            child: IconButton(
-              icon: Icon(Icons.arrow_forward, color: Colors.white, size: 32),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ImagePage()),
-                );
-              },
+            SizedBox(height: 20),
+            SmoothPageIndicator(
+              controller: _pageController,
+              count: 3,
+              effect: WormEffect(
+                dotColor: Colors.white.withOpacity(0.3),
+                activeDotColor: Colors.white,
+                dotHeight: 10,
+                dotWidth: 10,
+              ),
             ),
-          ),
-        ],
+            SizedBox(height: 20),
+            Align(
+              alignment: Alignment(0, 0.9),
+              child: IconButton(
+                icon: Icon(Icons.arrow_forward, color: Colors.white, size: 32),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SecondPage()),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildCard({required String imagePath, required String description}) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      color: Colors.white,
+      child: SizedBox(
+        width: 300,
+        height: 200,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset(
+                imagePath,
+                width: 300,
+                height: 150,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                description,
+                style: TextStyle(color: Colors.black, fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
-class ImagePage extends StatelessWidget {
+class SecondPage extends StatelessWidget {
+  final List<Map<String, String>> podcasts = [
+    {
+      'image': 'assets/images/1.jpg',
+      'title': 'Podcast 1',
+      'owner': 'Kişi 1',
+    },
+    {
+      'image': 'assets/images/2.jpg',
+      'title': 'Podcast 2',
+      'owner': 'Kişi 2',
+    },
+    {
+      'image': 'assets/images/3.jpg',
+      'title': 'Podcast 3',
+      'owner': 'Kişi 3',
+    },
+    {
+      'image': 'assets/images/4.jpg',
+      'title': 'Podcast 4',
+      'owner': 'Kişi 4',
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: TextField(
-          decoration: InputDecoration(
-            hintText: 'Arama Yap',
-            border: OutlineInputBorder(),
-          ),
+        backgroundColor: Color.fromARGB(255, 20, 10, 64),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              icon: Icon(Icons.menu, color: Colors.white),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+            Text(
+              'Podkes',
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+            IconButton(
+              icon: Icon(Icons.notifications, color: Colors.white),
+              onPressed: () {},
+            ),
+          ],
         ),
       ),
       body: GridView.builder(
@@ -82,11 +170,53 @@ class ImagePage extends StatelessWidget {
           mainAxisSpacing: 10,
           childAspectRatio: 1,
         ),
-        itemCount: 4,
+        itemCount: podcasts.length,
         itemBuilder: (context, index) {
-          return Image.asset(
-            'assets/images/${index + 1}.jpg',
-            fit: BoxFit.cover,
+          return GestureDetector(
+            onTap: () {
+              // Navigate to detailed podcast page
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => PodcastDetailPage(
+                    title: podcasts[index]['title']!,
+                    owner: podcasts[index]['owner']!,
+                    image: podcasts[index]['image']!,
+                  ),
+                ),
+              );
+            },
+            child: Card(
+              elevation: 5,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                      podcasts[index]['image']!,
+                      width: double.infinity,
+                      height: 150,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    podcasts[index]['title']!,
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  Text(
+                    podcasts[index]['owner']!,
+                    style: TextStyle(color: Colors.grey, fontSize: 14),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
           );
         },
       ),
@@ -98,6 +228,75 @@ class ImagePage extends StatelessWidget {
           BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profil'),
         ],
         onTap: (index) {},
+      ),
+    );
+  }
+}
+
+class PodcastDetailPage extends StatelessWidget {
+  final String title;
+  final String owner;
+  final String image;
+
+  PodcastDetailPage({
+    required this.title,
+    required this.owner,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 20, 10, 64),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Text(
+          title,
+          style: TextStyle(color: Colors.white, fontSize: 24),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset(
+                image,
+                width: double.infinity,
+                height: 250,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 16),
+            Text(
+              title,
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Text(
+              '$owner',
+              style: TextStyle(fontSize: 18, color: Colors.grey),
+            ),
+            SizedBox(height: 20),
+            LinearProgressIndicator(
+              value: 0.5,
+              color: Colors.blue,
+              backgroundColor: Colors.grey.shade300,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Podcast Oynatılıyor...',
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
       ),
     );
   }
